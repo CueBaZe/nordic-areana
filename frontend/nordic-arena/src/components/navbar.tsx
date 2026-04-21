c3073a3e6b71a508293029ade4d264d3e73aab34
CueBaZe <olivervalente@icloud.com>
2026-04-21 12:39:21 +0200

    Added a logo to the navbar

--- frontend/nordic-arena/src/components/navbar.tsx
+++ frontend/nordic-arena/src/components/navbar.tsx
@@ -1,9 +1,13 @@
 import { FaLock } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 export default function Navbar() {
     return (
         <div className="flex items-center justify-between h-20 bg-[#1E3A8A] px-6">
-            <h1 className="text-white text-4xl font-bold p-4">Nordic Arena</h1>
+            <div className="flex flex-row items-center">
+                <img src={logo} alt="logo" width={100} height={20} />
+                <h1 className="text-white text-4xl font-bold p-4">Nordic Arena</h1>
+            </div>
             <div className="flex flex-col items-center justify-center text-white text-center hover:cursor-pointer">
                 <FaLock className="text-2xl" />
                 <span className="text-md">Login</span>
