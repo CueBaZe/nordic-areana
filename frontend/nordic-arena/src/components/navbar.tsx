06f25a4b4cf8e38f992aaa462fc30aa04a9a83db
CueBaZe <olivervalente@icloud.com>
2026-04-23 13:57:25 +0200

    Made so you can navigate to the frontpage by clicking on the logo in the navbar

--- frontend/nordic-arena/src/components/navbar.tsx
+++ frontend/nordic-arena/src/components/navbar.tsx
@@ -1,11 +1,14 @@
 import { FaLock } from "react-icons/fa";
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
         <div className="flex items-center justify-between h-20 bg-[#1E3A8A] px-6">
             <div className="flex flex-row items-center">
-                <img src={logo} alt="logo" width={100} height={20} />
+                <Link to={'/'} className="transition duration-300 hover:cursor-pointer trans hover:scale-110">
+                    <img src={logo} alt="logo" width={100} height={20} />
+                </Link>
                 <h1 className="text-white text-4xl font-bold p-4">Nordic Arena</h1>
             </div>
             <div className="flex flex-col items-center justify-center text-white text-center hover:cursor-pointer">
