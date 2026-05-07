6bf6942c5a680264a29014c82a3922f251611e6d
CueBaZe <olivervalente@icloud.com>
2026-05-07 14:06:48 +0200

    Created payment table

---
+++ backend/nordic-arena-api/database/migrations/2026_05_07_120252_create_payment_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('booking_id')->constrained('bookings')->onDelete('cascade');
+            $table->string('paypay_order_id');
+            $table->integer('amount');
+            $table->string('currency');
+            $table->enum('status', ['pending', 'confirmed', 'cancelled'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment');
+    }
+};
