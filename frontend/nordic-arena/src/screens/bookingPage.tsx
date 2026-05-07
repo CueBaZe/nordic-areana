0a3ef0cd51c0e4de9b4a721b7da37b331878b2f1
CueBaZe <olivervalente@icloud.com>
2026-05-07 12:40:27 +0200

    Made the cards to the diffrent sports

--- frontend/nordic-arena/src/screens/bookingPage.tsx
+++ frontend/nordic-arena/src/screens/bookingPage.tsx
@@ -1,46 +1,107 @@
-import 'temporal-polyfill/global';
-import { ScheduleXCalendar, useCalendarApp } from "@schedule-x/react";
-import { createViewDay } from "@schedule-x/calendar"
 import Mainlayout from "../mainlayout";
+import footballImage from "../assets/football.jpg";
+import paddleImage from "../assets/paddle.jpg"
+import tennisImage from "../assets/tennis.jpg"
+import badmintonImage from "../assets/badminton.jpg"
+import bordtennisImage from "../assets/bordtennis.jpg"
 
 export default function BookingPage() {
 
-    const calender = useCalendarApp({
-        locale: 'da-DK',
-        views: [
-            createViewDay(),
-        ],
-        events: [
-            {
-                id: 1,
-                title: 'Paddle Bane 1',
-                start: Temporal.ZonedDateTime.from('2026-05-07 12:00[UTC]'),
-                end: Temporal.ZonedDateTime.from('2026-05-07 13:00[UTC]'),
-            }, 
-            {
-                id: 2,
-                title: 'Paddle Bane 2',
-                start: Temporal.ZonedDateTime.from('2026-05-07 12:00[UTC]'),
-                end: Temporal.ZonedDateTime.from('2026-05-07 13:00[UTC]'),
-            },
-            {
-                id: 3,
-                title: 'Paddle Bane 3',
-                start: Temporal.ZonedDateTime.from('2026-05-07 12:00[UTC]'),
-                end: Temporal.ZonedDateTime.from('2026-05-07 13:00[UTC]'),
-            },
-            {
-                id: 4,
-                title: 'Paddle Bane 4',
-                start: Temporal.ZonedDateTime.from('2026-05-07 12:00[UTC]'),
-                end: Temporal.ZonedDateTime.from('2026-05-07 13:00[UTC]'),
-            }
-        ],
-    });
 
     return (
         <Mainlayout> 
-            <ScheduleXCalendar calendarApp={calender}/>
+            <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-[25px] mt-4 mb-4">
+                <div className="bg-[#E0F2FE] border border-1 border-[#DBEAFE] rounded w-[350px] md:w-[450px] h-[400px] transition duration-300 hover:scale-105">
+                    <div className="flex w-full bg-[#1E3A8A] rounded-t h-[40px] items-center justify-center text-center">
+                        <h1 className="text-white text-2xl font-semibold">Padel</h1>
+                    </div>
+                    <div className="flex items-center justify-center mt-4">
+                        <img 
+                            src={paddleImage}
+                            alt="paddle" 
+                            className="w-[350px] h-[250px] object-cover rounded"
+                        />
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center text-center m-4 h-[60px]">
+                        <p className="text-[#0F176B] text-md md:text-lg">Booking af padelbane: Reservation sker i intervaller af 60 minutter. Lån af bat og bolde er inkluderet i prisen.</p>
+                    </div> 
+
+                </div>
+
+                <div className="bg-[#E0F2FE] border border-1 border-[#DBEAFE] rounded w-[350px] md:w-[450px] h-[400px] transition duration-300 hover:scale-105">
+                    <div className="flex w-full bg-[#1E3A8A] rounded-t h-[40px] items-center justify-center text-center">
+                        <h1 className="text-white text-2xl font-semibold">Tennis</h1>
+                    </div>
+                    <div className="flex items-center justify-center mt-4">
+                        <img 
+                            src={tennisImage}
+                            alt="paddle" 
+                            className="w-[350px] h-[250px] object-cover rounded"
+                        />
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center text-center m-4 h-[60px]">
+                        <p className="text-[#0F176B] text-md md:text-lg">Booking af tennisbane: Reservation sker i intervaller af 60 minutter. Lån af bat og bolde er inkluderet i prisen.</p>
+                    </div> 
+
+                </div>
+
+                <div className="bg-[#E0F2FE] border border-1 border-[#DBEAFE] rounded w-[350px] md:w-[450px] h-[400px] transition duration-300 hover:scale-105">
+                    <div className="flex w-full bg-[#1E3A8A] rounded-t h-[40px] items-center justify-center text-center">
+                        <h1 className="text-white text-2xl font-semibold">Badminton</h1>
+                    </div>
+                    <div className="flex items-center justify-center mt-4">
+                        <img 
+                            src={badmintonImage}
+                            alt="paddle" 
+                            className="w-[350px] h-[250px] object-cover rounded"
+                        />
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center text-center m-4 h-[60px]">
+                        <p className="text-[#0F176B] text-md md:text-lg">Booking af badmintonbane: Reservation sker i intervaller af 60 minutter. Lån af bat og bolde er inkluderet i prisen.</p>
+                    </div> 
+
+                </div>
+
+                <div className="bg-[#E0F2FE] border border-1 border-[#DBEAFE] rounded w-[350px] md:w-[450px] h-[400px] transition duration-300 hover:scale-105">
+                    <div className="flex w-full bg-[#1E3A8A] rounded-t h-[40px] items-center justify-center text-center">
+                        <h1 className="text-white text-2xl font-semibold">Bordtennis</h1>
+                    </div>
+                    <div className="flex items-center justify-center mt-4">
+                        <img 
+                            src={bordtennisImage}
+                            alt="paddle" 
+                            className="w-[350px] h-[250px] object-cover rounded"
+                        />
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center text-center m-4 h-[60px]">
+                        <p className="text-[#0F176B] text-md md:text-lg">Booking af bordtennisbane: Reservation sker i intervaller af 60 minutter. Lån af bat og bolde er inkluderet i prisen.</p>
+                    </div> 
+
+                </div>
+
+                <div className="bg-[#E0F2FE] border border-1 border-[#DBEAFE] rounded w-[350px] md:w-[450px] h-[400px] transition duration-300 hover:scale-105">
+                    <div className="flex w-full bg-[#1E3A8A] rounded-t h-[40px] items-center justify-center text-center">
+                        <h1 className="text-white text-2xl font-semibold">Fodbold</h1>
+                    </div>
+                    <div className="flex items-center justify-center mt-4">
+                        <img 
+                            src={footballImage}
+                            alt="paddle" 
+                            className="w-[350px] h-[250px] object-cover rounded"
+                        />
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center text-center m-4 h-[60px]">
+                        <p className="text-[#0F176B] text-md md:text-lg">Booking af fodboldbane: Reservation sker i intervaller af 60 minutter. Lån af bolde er inkluderet i prisen.</p>
+                    </div> 
+
+                </div>
+                
+            </div>
         </Mainlayout>
     );
 }
