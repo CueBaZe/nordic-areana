f169738e36c700f4717d4f112948e835e49fed5f
CueBaZe <olivervalente@icloud.com>
2026-05-11 10:46:47 +0200

    Made all booking pages so it can show timeslots

--- frontend/nordic-arena/src/mainlayout.tsx
+++ frontend/nordic-arena/src/mainlayout.tsx
@@ -1,5 +1,5 @@
 import { type ReactNode } from "react";
-import '@schedule-x/theme-default/dist/index.css'   
+import './index.css'
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 
--- frontend/nordic-arena/src/screens/bookingPages/bordtennis.tsx
+++ frontend/nordic-arena/src/screens/bookingPages/bordtennis.tsx
@@ -1,8 +1,45 @@
+import 'temporal-polyfill/global';
+import Calendar from "../../components/calendar";
+import type { CalendarEvent } from "../../components/calendar";
+import { useEffect, useState } from 'react';
 
-export default function Bordtennis() {
+
+export default function BordTennis() {
+    const today = Temporal.Now.plainDateISO().toString();
+    const [selectedDate, setSelectedDate] = useState<string>(today);
+    const [events, setEvents] = useState<CalendarEvent[]>([]);
+
+    const handleDateChange = (date: string) => { //gets the selected date from calendar component
+        setSelectedDate(date);
+    };
+
+    //Function that gets the timeslots from the backend
+    useEffect(() => {
+        const fetchTimeSlots = async () => {
+            const result = await fetch(`http://127.0.0.1:8000/api/getTimeSlots?date=${selectedDate}&type=bordtennis `, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+            });
+
+            if (!result.ok) {
+                console.log('Error fetching Timeslots');
+                return;
+            }
+
+            const data = await result.json();
+
+            setEvents(data);
+        }
+
+        fetchTimeSlots();
+    }, [selectedDate])
+
     return (
         <div>
-            <p>Bordtennis</p>
+            <Calendar initialEvents={events} onDateChange={handleDateChange}/>
         </div>
     );
 }
