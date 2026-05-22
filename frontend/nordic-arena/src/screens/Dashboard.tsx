62c6729c2bb53c9da024bd4f61366077aea777a8
CueBaZe <olivervalente@icloud.com>
2026-05-22 11:06:31 +0200

    Fixed some responsiveness styling

--- frontend/nordic-arena/src/screens/Dashboard.tsx
+++ frontend/nordic-arena/src/screens/Dashboard.tsx
@@ -8,17 +8,17 @@ export default function Dashboard() {
   return (
     <div className="flex flex-col min-h-screen">
       <Mainlayout>
-        <div className="flex flex-col mt-8 mb-8 md:m-0 md:flex-row gap-[50px] md:gap-[250px]">
+        <div className="flex flex-col items-center justify-center mt-8 mb-8 md:m-0 lg:flex-row gap-[50px] lg:gap-[200px]">
           <Link to="/booking">
-            <div className="bg-[#E0F2FE] border border-1 border-[#DBEAFE] rounded w-[450px] h-[400px] transition duration-300 hover:scale-105">
+            <div className="bg-[#E0F2FE] border border-1 border-[#DBEAFE] rounded w-[400px] md:w-[450px] h-[400px] transition duration-300 hover:scale-105">
               <div className="flex w-full bg-[#1E3A8A] rounded-t h-[40px] items-center justify-center text-center">
                 <h1 className="text-white text-2xl font-semibold">Book baner</h1>
               </div>
               <div className="flex items-center justify-center mt-4">
                 <img 
                   src={sportImage} 
-                  alt="sport" 
-                  className="w-[350px] h-[250px] object-cover rounded"
+                  loading="lazy"
+                  className="w-[350px] h-[250px] object-cover rounded border border-2 border-[#DBEAFE]"
                 />
               </div>
 
@@ -29,15 +29,15 @@ export default function Dashboard() {
           </Link>
 
           <Link to="/bookninger">
-            <div className="bg-[#E0F2FE] border border-1 border-[#DBEAFE] rounded w-[450px] h-[400px] transition duration-300 hover:scale-105">
+            <div className="bg-[#E0F2FE] border border-1 border-[#DBEAFE] rounded w-[400px] md:w-[450px] h-[400px] transition duration-300 hover:scale-105">
               <div className="flex w-full bg-[#1E3A8A] rounded-t h-[40px] items-center justify-center text-center">
                 <h1 className="text-white text-2xl font-semibold">Dine baner</h1>
               </div>
               <div className="flex items-center justify-center mt-4">
                 <img 
                   src={baneImage} 
-                  alt="sport" 
-                  className="w-[350px] h-[250px] object-cover rounded"
+                  loading="lazy"
+                  className="w-[350px] h-[250px] object-cover rounded border border-2 border-[#DBEAFE]"
                 />
               </div>
 
