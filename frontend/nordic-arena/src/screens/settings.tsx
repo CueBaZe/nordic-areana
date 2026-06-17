0576ba1658c62990d429059dde44d8c460904a4f
CueBaZe <olivervalente@icloud.com>
2026-06-17 13:32:22 +0200

    Made some ui changes to and added so you can chane from infomation settings to password settings

--- frontend/nordic-arena/src/screens/settings.tsx
+++ frontend/nordic-arena/src/screens/settings.tsx
@@ -2,14 +2,35 @@ import { useState } from "react";
 import Mainlayout from "../mainlayout";
 import { FaUser, FaPhone } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
+import { GrShieldSecurity } from "react-icons/gr";
+import { useAuth } from "../components/authContext";
 
 export default function Settings() {
     
     const [shownPage, setShownPage] = useState<string>('infomation');
+    const { user } = useAuth();
 
     return (
         <Mainlayout>
-            <div className="flex flex-col md:flex-row gap-[25px] justify-center items-center text-center text-[#0F176B]">
+            <div className="flex flex-col gap-[25px] justify-center items-center text-center text-[#0F176B]">
+
+                <div className="flex flex-row w-[400px] h-[50px] bg-[#E0F2FE] rounded-xl items-center justify-between">
+                    <div className="bg-[#1E3A8A] w-[10px] h-full rounded-l-xl"></div>
+                    {shownPage === 'infomation' ? (
+                        <FaUser size={24} color="#17229b"/>
+                    ) : (
+                        <FaUser onClick={() => {setShownPage('infomation')}} size={20} color="#0F176B" title="Infomation" className="cursor-pointer"/>
+                    )}
+
+                    {shownPage === 'password' ? (
+                        <GrShieldSecurity size={24} color="#17229b"/>
+                    ) : (
+                        <GrShieldSecurity onClick={() => {setShownPage('password')}} size={20} color="#0F176B" title="Password" className="cursor-pointer"/>
+                    )}
+
+                    <div className="bg-[#1E3A8A] w-[10px] h-full rounded-r-xl"></div>
+                </div>
+
                 <div className="flex flex-col bg-[#E0F2FE] border border-1 border-[#DBEAFE] rounded-lg w-[300px] md:w-[450px] h-[500px] overflow-y-auto">
                     <div className="text-center bg-[#1E3A8A] rounded-t-lg p-2"> {/* Header */}
                         <h1 className="text-2xl text-white font-semibold">Settings ({shownPage})</h1>
@@ -21,17 +42,23 @@ export default function Settings() {
                                 <div className="flex flex-col items-center justify-center gap-16 mt-8">
                                     <div className="flex flex-row gap-2 justify-center items-center border border-1 border-[#C5D3E5] rounded-lg p-1"> {/* Name input */}
                                         <FaUser size={14} color="#0F176B"/>
-                                        <input className="text-xl" type="text" placeholder="Navn" />
+                                        <input className="text-xl" type="text" placeholder={user?.name} />
                                     </div>
 
                                     <div className="flex flex-row gap-2 justify-center items-center border border-1 border-[#C5D3E5] rounded-lg p-1"> {/* Email input */}
                                         <MdEmail size={14} color="#0F176B"/>
-                                        <input className="text-xl" type="text" placeholder="Email" />
+                                        <input className="text-xl" type="text" placeholder={user?.email} />
                                     </div>
 
                                     <div className="flex flex-row gap-2 justify-center items-center border border-1 border-[#C5D3E5] rounded-lg p-1"> {/* Phone input */}
                                         <FaPhone size={14} color="#0F176B"/>
-                                        <input className="text-xl" type="text" placeholder="Telefon Nummer" />
+                                        <input className="text-xl" type="text" placeholder={user?.phone} />
+                                    </div>
+
+                                    <div>
+                                        <button className="bg-green-400 rounded-xl text-white text-xl font-semibold p-2 w-[100px] transtion-transform duration-300 hover:scale-110 cursor-pointer">
+                                            Gem
+                                        </button>
                                     </div>
                                 </div>
                             </>
@@ -52,6 +79,12 @@ export default function Settings() {
 
                                     <div className="flex flex-row gap-2 justify-center items-center border border-1 border-[#C5D3E5] rounded-lg p-1"> {/* repeat password */}
                                         <input className="text-lg" type="password" placeholder="Gentag password"/>
+                                    </div>
+
+                                    <div>
+                                        <button className="bg-green-400 rounded-xl text-white text-xl font-semibold p-2 w-[100px] transtion-transform duration-300 hover:scale-110 cursor-pointer">
+                                            Gem
+                                        </button>
                                     </div>
                                 </div>
                             </>
